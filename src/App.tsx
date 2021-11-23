4769063b483f13703d3e64ddc08d2e292f0991bb
I-the-Creator <andrey.burluckiy@gmail.com>
2021-11-23 22:05:29 +0100

    types added to API.ts and App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+//Routing 
+//@ts-ignore
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Components
+import Header from './components/Header';
+import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound'
+
+// Styles
+import { GlobalStyle } from './GlobalStyle';
+
+const App: React.FC = () =>  (
+    <Router>   {/* wrap whole app into the Router */}
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />  {/* first route to 'Home' */}
+        <Route path='/:movieId' element={<Movie />} />  {/* route to movie page by movieID (this param name can be changed) */}
+        <Route path='/*' element={<NotFound />} />  {/* route to NotFound page  - any page that doesn't exist*/}
+      </Routes>
+      <GlobalStyle/>
+    </Router>
+  );
+
+export default App;
