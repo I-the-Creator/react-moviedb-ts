db4003a0a9c44d91ec9113a22d1e2f386ce9b07a
I-the-Creator <andrey.burluckiy@gmail.com>
2021-11-25 23:19:31 +0100

    types added to Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,16 +4,17 @@ import PropTypes from 'prop-types';
 //Styles
 import { Wrapper } from './Button.styles';
 
-const Button = ({ text, callback}) => (  /*  destructuring props */
+//Types
+type Props = {   //any name can be used instead of Props
+    text: string;
+    callback: () => void; // click callback
+}
+
+const Button: React.FC<Props> = ({ text, callback}) => (  /*  destructuring props */
 
     <Wrapper type="button" onClick={callback}>
         {text}
     </Wrapper>
 );
-
-Button.propTypes = {
-    text: PropTypes.string,
-    callback: PropTypes.func
-}
 
 export default Button;
